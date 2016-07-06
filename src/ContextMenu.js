c02a5a9ccdab74e80a67619f75c14f0e264ef9ff
Thomas Sojka <thomas.sojka@comsysto.com>
2016-07-06 18:51:49 +0200

    Extract ContextMenu into own file

---
+++ src/ContextMenu.js
@@ -0,0 +1,46 @@
+let contextMenu
+let contextMenuOpen
+let marker
+
+exports.create = (game, layer) => {
+  marker = game.add.graphics()
+  marker.lineStyle(2, 0xffffff, 1)
+  marker.drawRect(0, 0, 64, 64)
+  game.input.addMoveCallback(() => updateMarker(game, layer), this)
+}
+
+exports.open = (menuEntries, game, layer, {x, y}) => {
+  if (contextMenuOpen) {
+    contextMenuOpen = false
+    contextMenu.destroy()
+    updateMarker(game, layer)
+  } else {
+    createContextMenu(game, menuEntries, {x, y})
+    contextMenuOpen = true
+  }
+}
+
+const createContextMenu = (game, menuEntries, {x, y}) => {
+  contextMenuOpen = true
+  contextMenu = game.add.group()
+  menuEntries.forEach(({text, action}, index) => {
+    let contextMenuEntry = game.add.group()
+    const button = contextMenuEntry.create(x, y + index * 10, 'button')
+    button.inputEnabled = true
+    button.events.onInputDown.add(() => {
+      menuEntries[index].action(marker)
+      contextMenuOpen = false
+      contextMenu.destroy()
+    })
+    contextMenuEntry.add(game.add.text(x + 70, y + 10 + index * 10, text))
+    contextMenu.add(contextMenuEntry)
+  })
+}
+
+function updateMarker (game, layer) {
+  if (!contextMenuOpen) {
+    marker.x = layer.getTileX(game.input.activePointer.worldX) * 32
+    marker.y = layer.getTileY(game.input.activePointer.worldY) * 32
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 const Student = require('./Student')
+const ContextMenu = require('./ContextMenu')
 const { washingMachineState } = require('./constants')
 
 const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload, create, update })
 
 let map
 let layer
-let contextMenu
 let washingMachines = []
-let marker
-let contextMenuOpen
 
 const menuEntries = [
   {
     text: 'Buy',
-    action (x, y) {
-      const washingMachine = game.add.sprite(marker.x + 6.4, marker.y, 'washingMachine')
-      washingMachines.push(washingMachine)
-      game.physics.arcade.enable(washingMachine)
-      washingMachine.scale.setTo(0.1, 0.1)
-      washingMachine.body.enable = true
-      washingMachine.body.immovable = true
-      washingMachine.state = washingMachineState.FREE
-      contextMenuOpen = false
-      contextMenu.destroy()
-    }
+    action: buyWashingMachine
   }
 ]
 
@@ -35,6 +23,16 @@ function preload () {
   game.load.spritesheet('student', 'assets/student.png', 57, 99, 14)
 }
 
+function buyWashingMachine (marker) {
+  const washingMachine = game.add.sprite(marker.x + 6.4, marker.y, 'washingMachine')
+  washingMachines.push(washingMachine)
+  game.physics.arcade.enable(washingMachine)
+  washingMachine.scale.setTo(0.1, 0.1)
+  washingMachine.body.enable = true
+  washingMachine.body.immovable = true
+  washingMachine.state = washingMachineState.FREE
+}
+
 function create () {
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
@@ -42,46 +40,12 @@ function create () {
   map.addTilesetImage('terrain', 'tiles')
   layer = map.createLayer('Tile Layer 1')
   layer.inputEnabled = true
-  layer.events.onInputDown.add(onTileClicked)
-
-  marker = game.add.graphics()
-  marker.lineStyle(2, 0xffffff, 1)
-  marker.drawRect(0, 0, 64, 64)
-  game.input.addMoveCallback(updateMarker, this)
+  layer.events.onInputDown.add((_, {clientX, clientY}) =>
+                               ContextMenu.open(menuEntries, game, layer, {x: clientX, y: clientY}))
+  ContextMenu.create(game, layer)
   Student.create(game)
-}
-
-function updateMarker () {
-  if (!contextMenuOpen) {
-    marker.x = layer.getTileX(game.input.activePointer.worldX) * 32
-    marker.y = layer.getTileY(game.input.activePointer.worldY) * 32
-  }
 }
 
 function update () {
   Student.update(game, washingMachines)
-}
-
-function onTileClicked (tile, event) {
-  if (contextMenuOpen) {
-    contextMenuOpen = false
-    contextMenu.destroy()
-    updateMarker()
-  } else {
-    createContextMenu(menuEntries, {x: event.clientX, y: event.clientY})
-    contextMenuOpen = true
-  }
-}
-
-const createContextMenu = (menuEntries, {x, y}) => {
-  contextMenuOpen = true
-  contextMenu = game.add.group()
-  menuEntries.forEach(({text, action}, index) => {
-    let contextMenuEntry = game.add.group()
-    const button = contextMenuEntry.create(x, y + index * 10, 'button')
-    button.inputEnabled = true
-    button.events.onInputDown.add(menuEntries[index].action.bind({}, x, y))
-    contextMenuEntry.add(game.add.text(x + 70, y + 10 + index * 10, 'Buy'))
-    contextMenu.add(contextMenuEntry)
-  })
 }
