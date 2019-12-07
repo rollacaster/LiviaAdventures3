122d2a24ed39701ad3a74044f14879d74aaad2a8
Thomas Sojka <thomas.sojka@comsysto.com>
2019-12-07 17:27:49 +0100

    Update webpack

--- bundlerConfig.js
+++ bundlerConfig.js
@@ -2,19 +2,21 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: [
-    './src/index'
-  ],
+  entry: ['./src/index'],
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'game.js'
   },
   plugins: [
-    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
     process.env.NODE_ENV === 'production'
-      ? new webpack.optimize.UglifyJsPlugin({minimize: true})
-    : () => {}
+      ? new webpack.optimize.UglifyJsPlugin({ minimize: true })
+      : () => {}
   ],
-  devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : 'cheap-module-source-map',
-  debug: process.env.NODE_ENV !== 'production'
+  devtool:
+    process.env.NODE_ENV !== 'production'
+      ? 'source-map'
+      : 'cheap-module-source-map'
 }
